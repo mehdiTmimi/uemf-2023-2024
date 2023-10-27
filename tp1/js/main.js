b2644b9b8fd754c368bcd7745e25d911759f8c95
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-10-27 12:20:47 +0100

    exercice

--- tp1/js/main.js
+++ tp1/js/main.js
@@ -1,4 +1,5 @@
-// creations des elements
+let f1=()=>{
+    // creations des elements
 let div = document.createElement("div")
 let h1 = document.createElement("h1")
 let p = document.createElement("p")
@@ -13,4 +14,30 @@ p.innerText="  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explica
 
 // lier avec l'arbre Dom
 let content=document.getElementById('content')
+content.innerHTML=""
 content.appendChild(div)
+}
+let f2=()=>{
+    let x =10;
+    console.log(x)
+}
+
+//en utilisant les attributs (statique) ajouter un 
+// evenement click au H1
+let h1=document.getElementById("title")
+h1.setAttribute('onclick','alert("salut")')
+
+//---------
+let btn2=document.getElementById('btn2')
+let callback1=()=>{
+    console.log('click dynamique');
+}
+btn2.addEventListener('click',callback1)
+btn2.addEventListener('click',()=>{
+    console.log('click dynamique personnaalisee');
+})
+btn2.addEventListener('click',()=>{
+    f1()
+})
+
+btn2.removeEventListener('click',callback1)

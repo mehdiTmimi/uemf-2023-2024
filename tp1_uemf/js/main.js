12237c821c7be768431f0d0699d197f8b4d8184a
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-03 11:45:56 +0100

    ok

--- tp1_uemf/js/main.js
+++ tp1_uemf/js/main.js
@@ -14,3 +14,34 @@ let vider = ()=>{
 annulerBtn.addEventListener('click',()=>{
     vider();
 })
+
+let addEtudiant =(nom,prenom,age)=>{
+    // creation des elements
+    let ligne = document.createElement('tr')
+    let nomTd= document.createElement('td')
+    let prenomTd= document.createElement('td')
+    let ageTd= document.createElement('td')
+    let optionTd= document.createElement('td')
+    let deleteBtn= document.createElement('button')
+
+
+    // les valeurs
+    nomTd.innerText=nom;
+    prenomTd.innerText=prenom;
+    ageTd.innerText=age;
+    deleteBtn.innerText="X";
+    deleteBtn.classList.add('delete')
+
+    tbody.appendChild(ligne);
+    ligne.appendChild(nomTd)
+    ligne.appendChild(prenomTd)
+    ligne.appendChild(ageTd)
+    ligne.appendChild(optionTd)
+    optionTd.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener('click',()=>{
+        ligne.remove()
+    })
+}
+addEtudiant("tmimi",'mehdi',31)
+addEtudiant("filali",'hamza',12)
